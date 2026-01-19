9ab8883015f075356846ee1163ff789c0c16ad7c
sydestes12 <99297020+sydestes12@users.noreply.github.com>
2026-01-19 11:12:29 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Sydney Estes";
+export const SITE_IMAGE = "";
 export const SITE_DESCRIPTION = "Hi I'm Sydney!";
